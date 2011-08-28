016d257ec42fe92de174f97f80a7746c4339d8b0
Michael Holm <hollo@hollo.dk>
2011-08-29 00:07:33 +0200

    Renamed controller files.

--- Controller/AdminZoneController.php
+++ Controller/AdminZoneController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    
+
     public function indexAction($name)
     {
         return $this->render('HolloBindBundle:Default:index.html.twig', array('name' => $name));
