c89f41ba01074f447190425114d992b4b7511905
Michael Holm <hollo@hollo.dk>
2011-08-29 00:13:16 +0200

    Added AdminZone controller.

--- Controller/AdminZoneController.php
+++ Controller/AdminZoneController.php
@@ -3,13 +3,49 @@
 namespace Hollo\BindBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
-class DefaultController extends Controller
+class AdminZoneController extends Controller
 {
+  /**
+   * @Template()
+   * @Route("/zone")
+   */
+  public function indexAction()
+  {
+  }
 
-    public function indexAction($name)
-    {
-        return $this->render('HolloBindBundle:Default:index.html.twig', array('name' => $name));
-    }
+  /**
+   * @Template()
+   * @Route("/zone/new")
+   */
+  public function newAction()
+  {
+  }
+
+  /**
+   * @Template()
+   * @Route("/zone/update/{id}")
+   */
+  public function updateAction($id)
+  {
+  }
+
+  /**
+   * @Template()
+   * @Route("/zone/delete/{id}")
+   */
+  public function deleteAction($id)
+  {
+  }
+
+  /**
+   * @Template()
+   * @Route("/zone/search")
+   */
+  public function searchAction()
+  {
+  }
 }
