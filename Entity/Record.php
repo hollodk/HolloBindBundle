d3b3584f277283977a260cfcb8ace130b21b8a02
Michael Holm <hollo@hollo.dk>
2011-08-29 13:30:02 +0200

    Added record entityt.

---
+++ Entity/Record.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Hollo\BindBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Hollo\BindBundle\Entity\Record
+ *
+ * @ORM\Table(name="dns_record")
+ * @ORM\Entity(repositoryClass="Hollo\BindBundle\Entity\RecordRepository")
+ */
+class Record
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string $address
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @var string $type
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Domain")
+     */
+    private $domain;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set domain
+     *
+     * @param Hollo\BindBundle\Entity\Domain $domain
+     */
+    public function setDomain(\Hollo\BindBundle\Entity\Domain $domain)
+    {
+        $this->domain = $domain;
+    }
+
+    /**
+     * Get domain
+     *
+     * @return Hollo\BindBundle\Entity\Domain
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+}
