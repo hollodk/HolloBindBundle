5abcf73acf0621061db291163ac90ea12c8b56a3
Michael Holm <hollo@hollo.dk>
2011-08-29 12:17:33 +0200

    Fixed add zone command.

--- Command/ZoneAddCommand.php
+++ Command/ZoneAddCommand.php
@@ -14,10 +14,34 @@ class ZoneAddCommand extends ContainerAwareCommand
   {
     $this
       ->setName('bind:zone:add')
-      ->setDescription('Add zone to Bind');
+      ->setDescription('Add a new zone.')
+      ->setDefinition(array(
+        new InputArgument('domain', InputArgument::REQUIRED, 'The domain'),
+        new InputArgument('address', InputArgument::REQUIRED, 'The address'),
+        new InputArgument('password', InputArgument::REQUIRED, 'The password'),
+        new InputArgument('description', InputArgument::REQUIRED, 'The description'),
+      ));
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
+    $domain = $input->getArgument('domain');
+    $address = $input->getArgument('address');
+    $password = $input->getArgument('password');
+    $description = $input->getArgument('description');
+
+    $queue = new \Hollo\BindBundle\Entity\AddQueue();
+    $queue->setDomain($domain);
+    $queue->setAddress($address);
+    $queue->setPassword($password);
+    $queue->setDescription($description);
+    $queue->setNs1('ns1.hollo.dk');
+    $queue->setNs2('ns2.hollo.dk');
+
+    $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+    $em->persist($queue);
+    $em->flush();
+
+    $output->writeln(sprintf('Added zone <comment>%s</comment>', $domain));
   }
 }
