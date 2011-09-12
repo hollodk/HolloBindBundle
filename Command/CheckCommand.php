aca3a9aa544151b8c6800b43f2fe4850177c5bf2
Michael Holm <hollo@hollo.dk>
2011-09-12 12:00:38 +0200

    Added check command.

--- Command/CheckCommand.php
+++ Command/CheckCommand.php
@@ -20,7 +20,43 @@ class CheckCommand extends ContainerAwareCommand
   protected function execute(InputInterface $input, OutputInterface $output)
   {
     $bind = $this->getContainer()->get('hollo_bind.bind');
+    $errors = array();
 
-    $bind->checkEnvironment();
+    if (!file_exists($bind->getZonePath())) {
+      $errors[] = 'Zone path does not exists.';
+      $errors[] = '<comment>mkdir -p '.$bind->getZonePath().'</comment>';
+
+    } elseif (!is_writeable($bind->getZonePath())) {
+      $errors[] = 'Zone path is not writeable.';
+      $errors[] = '<comment>chmod 740 '.$bind->getZonePath().'</comment>';
+    }
+
+    if (!file_exists($bind->getConfigPath())) {
+      $errors[] = 'Config path does not exists.';
+      $errors[] = '<comment>mkdir -p '.$bind->getConfigPath().'</comment>';
+
+    } elseif (!file_exists($bind->getConfigPath().'/'.$bind->getConfigFile())) {
+      $errors[] = 'Config file does not exists.';
+      $errors[] = '<comment>touch '.$bind->getConfigPath().'/'.$bind->getConfigFile().'</comment>';
+
+    } elseif (!is_writeable($bind->getConfigPath().'/'.$bind->getConfigFile())) {
+      $errors[] = 'Config file is not writeable.';
+      $errors[] = '<comment>chmod 740 '.$bind->getConfigPath().'/'.$bind->getConfigFile().'</comment>';
+    }
+
+    if (!file_exists($bind->getBindInit()))
+      $errors[] = 'Init file not found.';
+
+    // check init
+    // exists
+
+    if (count($errors)) {
+      $output->writeln('<error>Your setup is not ready yet, validate the following:</error>');
+      foreach ($errors as $error) {
+        $output->writeln($error);
+      }
+    } else {
+      $output->writeln('<info>Everything seems just fine :)</info>');
+    }
   }
 }
